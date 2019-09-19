9475fca999dd153418b75080c1ce16188e9c4148
Andy Kopp <andreasrkopp@gmail.com>
2019-09-19 18:21:48 +0200

    update

--- running_code/kinect_hugme_1909.py
+++ running_code/kinect_hugme_1909.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python
 import freenect
 import cv2
@@ -6,19 +5,19 @@ import frame_convert2
 import numpy as np
 import math, time, io, sys
 from collections import deque
-import gpiozero
 from time import sleep
+import gpiozero
+
+
 
 ### GLOBAL VARIABLES
-kernel = np.ones((3,3), np.uint8) #small structuring element
-kernel_big = np.ones((9,9), np.uint8) #big structuring element
-
-#here you can adjust the parameter of the background substract, change the second variable lower to make it more sensitive
-backSub = cv2.createBackgroundSubtractorKNN(history=10000, dist2Threshold=50, detectShadows=False)
-
-#get more contrast out of the image
-clahe = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-
+KERNEL = np.ones((3,3), np.uint8)
+KERNEL_BIG = np.ones((9,9), np.uint8)
+# Create two independent background substractors, because RGB and depth image might need different parameters:
+# NOTE: ADAPT THE RGB SUBSTRACTOR PARAMETERS ON THE LOCATION YOU SET UP THE KINECT TO GET BEST RECOGNITION:
+backSubDepth = cv2.createBackgroundSubtractorKNN(history=500, dist2Threshold=50, detectShadows=0)
+backSubRgb = cv2.createBackgroundSubtractorKNN(history=1000, dist2Threshold=50, detectShadows=0) # use default parameters
+#backSub = cv2.createBackgroundSubtractorMOG2() # performed worse then KNN
 CACHE_SIZE = 4 # size of the list that stores previous distance values, must be 4 or greater
 if CACHE_SIZE < 4: CACHE_SIZE = 4
 pre_distances = deque([10000] * CACHE_SIZE) # stores previous distances of the two biggest blobs to recognize valid movement
@@ -28,13 +27,13 @@ IMG_DEPTH = 0
 IMG_RGB = 1
 THRESHOLD = 814
 DEPTH = 152
-TIME_BETWEEN_FRAMES = 0.3 # good values for testing .3 (fast), 1 (slow)
+TIME_BETWEEN_FRAMES = 0.1 # good values for testing .3 (fast), 1 (slow)
 RELAY_PIN = 21
 relay = gpiozero.OutputDevice(RELAY_PIN, active_high=False, initial_value=False)
 
 ### CLASSES
 class Blob:
-    def __init__(self, x, y, width, height, size, x_center, y_center):
+    def __init__(self, x=0, y=0, width=0, height=0, size=0, x_center=0, y_center=0):
         self.x = x
         self.y = y
         self.width = width
@@ -52,11 +51,12 @@ class Blob:
     @classmethod
     def getBlobs(cls, labels, stats, centroids):
         blobs = [0] * 2
-        blobs[0] = Blob(0,0,0,0,0,0,0)
-        blobs[1] = Blob(0,0,0,0,0,0,0)
+        blobs[0] = Blob()
+        blobs[1] = Blob()
         i = 1
-    
-        for label in range(0, np.max(labels)): # ignore first label, which is the background
+        
+        # Note: One of the labels is the "background" label which has the size of the whole image
+        for label in range(0, np.max(labels)):
             size = stats[label, cv2.CC_STAT_AREA]
             x = stats[label, cv2.CC_STAT_LEFT]
             y = stats[label, cv2.CC_STAT_TOP]
@@ -85,69 +85,43 @@ class Blob:
         return blobs
 
 ### METHODS
-def get_img(mode):
+def get_img(mode):#, #showRaw=0):
     # This was intended to inhibit the stream warnings to the console, but it did not work:
     #text_trap = io.StringIO()
     #sys.stderr = text_trap
     if (mode == IMG_RGB):
-        # gets one frame from the RGB camera
-        frame = freenect.sync_get_video()[0]
-
-        #frame = clahe.apply(frame)
-
+        frame = freenect.sync_get_video()[0] # gets the Kinect RGB image
         
+
         #background subsraction
         #Learning Rate Parameter / -1 auto, 0 not updated at all, 1 new from last frame
-        fgMask = backSub.apply(frame, learningRate=-1) 
+        fgMask = backSubRgb.apply(frame, learningRate=-1)
         
         #threshold to get rid of any other color then black and white
         #anything lighter then 127 will be set to 255 (white) anything lower to 0 (black)
         # we dont need this if we turn shadows off
         #thresold expects a single channel image // with shadows enabled its a greyscale image
-        #ret, fgMask = cv2.threshold(fgMask,127,255,cv2.THRESH_BINARY)
 
-        #erosion
-        #fgMask = cv2.erode(fgMask, kernel, iterations = 1)
 
-        #dilation
+        ret, fgMask = cv2.threshold(fgMask,127,255,cv2.THRESH_BINARY)
+        
 
-        #fgMask = cv2.dilate(fgMask, kernel, iterations = 1)
-     
-     
-        fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_CLOSE, kernel_big) # closes gaps smaller than 9x9 pixels
-
-        #change color space from grayscale to BGR so we can draw a colored box later around blobs
-        #col = cv2.cvtColor(fgMask, cv2.COLOR_GRAY2BGR)
-
+        fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_CLOSE, KERNEL_BIG) # closes gaps smaller than 9x9 pixels 
     elif (mode == IMG_DEPTH):
         frame = freenect.sync_get_depth()[0] # gets the Kinect depth image
-        frame = 255 * np.logical_and(frame >= DEPTH - THRESHOLD,
+        fgMask = 255 * np.logical_and(frame >= DEPTH - THRESHOLD,
                                  frame <= DEPTH + THRESHOLD)
-
-        # we make sure its a 8-bit single-channel image // do we need this
-        #frame = frame.astype(np.uint8)
-
-        #background subsration // do we need this? // we do this already with the threshold
-        fgMask = backSub.apply(frame, learningRate=-1)
-
-        #do we need this // there are not grey colors in the depth image after the threshold
+        fgMask = fgMask.astype(np.uint8)
+        
+        fgMask = backSubDepth.apply(fgMask, learningRate=-1)
+        # It looks like these operations are not needed for the depth image:
         #ret, fgMask = cv2.threshold(fgMask,127,255,cv2.THRESH_BINARY)
-
-        #erosion
-        #fgMask = cv2.erode(fgMask, kernel, iterations = 1) # morphological erode with 3x3
         
-        #dilation
-
-
+        fgMask = cv2.erode(fgMask, KERNEL, iterations = 1) # morphological erode with 3x3
         
-        #fgMask = cv2.dilate(fgMask, kernel, iterations=1)
+        fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_CLOSE, KERNEL_BIG) # closes gaps smaller than 9x9 pixels 
 
-        # do we need this
-        fgMask = cv2.morphologyEx(fgMask, cv2.MORPH_CLOSE, kernel_big) # closes gaps smaller than 9x9 pixels
-
-        #change color space from grayscale to BGR so we can draw a colored box later around blobs
-        #col = cv2.cvtColor(fgMask, cv2.COLOR_GRAY2BGR)
-
+    #if showRaw == 1: cv2.imshow('Raw', frame)
     # Problem: this function gives us sometimes only one blob instead of two
     ret, labels, stats, centroids = cv2.connectedComponentsWithStats(fgMask)
     
@@ -178,18 +152,19 @@ def checkHugEvent(blobs):
     #        pre_distances[i] = round(pre_distances[i-1] + pre_distances[i+1] / 2) # interpolate unrecognized distance
 
     # When only one blob has been recognized this time, check if previous distances fit the proper motion:
-    if distance == 10000:
+    if distance == 10000 and pre_distances[CACHE_SIZE - 1] < 150:
         for i in range(0, CACHE_SIZE - 1):
             print("pre %d: %d, pre %d: %d" % (i, pre_distances[i], i+1, pre_distances[i + 1]))
             if pre_distances[i] <= pre_distances[i + 1]:
                 valid = 0
                 break
     else:
-        print("no event")
-        pre_distances.append(distance)
-        pre_distances.popleft()
+        print("NO EVENT")
         print(pre_distances)
         valid = 0
+
+    pre_distances.append(distance)
+    pre_distances.popleft()
 
     if valid == 1:
         sleep(0.5) # maybe wait 1 second,
@@ -198,19 +173,20 @@ def checkHugEvent(blobs):
         relay.on() # here the relay will be turned on
         sleep(0.5)
         relay.off()
+        sleep(15)
         relay.on()
-        sleep(20)
+        sleep(0.5)
         relay.off()
 
         #dummy = raw_input("Press key for next loop...") # Warten auf Tastatur, muss im Realbetrieb aus.
         pre_distances = deque([10000] * CACHE_SIZE) # reset previous distances
-
-
+        
 def show_video():
     cv2.imshow('Video', frame_convert2.video_cv(frame))
 
 ### INIT
 # Activate windows only for debug: 
+#cv2.namedWindow('Raw')
 #cv2.namedWindow('Video')
 cv2.namedWindow('FGMask')
 #cv2.namedWindow('FGMaskRaw')
@@ -221,28 +197,28 @@ print('Press ESC in window to stop')
 # It seems the first captures are wrong and/or the library needs some time
 # to initialize the background image, so some images are skipped at start:
 for num in range(1,10):
-    #get_img(IMG_DEPTH)
-    get_img(IMG_RGB)
+    get_img(IMG_DEPTH)
+    #get_img(IMG_RGB)
 
 ### LOOP
 while 1:
     #relay.toggle() #test relay
-    ret, frame, fgMask, labels, stats, centroids = get_img(IMG_RGB) # switch back to IMG_RGB
-    #ret, frame, fgMask, labels, stats, centroids = get_img(IMG_DEPTH)
+    #ret, frame, fgMask, labels, stats, centroids = get_img(IMG_RGB, showRaw=1)
+    ret, frame, fgMask, labels, stats, centroids = get_img(IMG_DEPTH)#, showRaw=1)
     print("\033[H\033[J") # clear screen
     blobs = Blob.getBlobs(labels, stats, centroids)
-    # If no blob was found, try depth image:
-    #if blobs[0].width == 0 and blobs[0].height == 0 and blobs[1].width == 0 and blobs[1].height == 0:
-    #    ret, frame, fgMask, labels, stats = get_img(IMG_DEPTH)
-    #    blobs = Blob.getBlobs(labels, stats)
+    # If no blob was found, try RGB image:
+    if blobs[0].width == 0 and blobs[0].height == 0 and blobs[1].width == 0 and blobs[1].height == 0:
+        ret, frame, fgMask, labels, stats, centroids = get_img(IMG_RGB)#, showRaw=1)
+        blobs = Blob.getBlobs(labels, stats, centroids)
 
-    checkHugEvent(blobs) 
-    sleep(TIME_BETWEEN_FRAMES)
-    #dummy = raw_input("Press key for next loop...")
- #  labeled_img = imshow_components(fgMask)
- #  cv2.imshow('Labels', labeled_img)
+    checkHugEvent(blobs)
+    #sleep(TIME_BETWEEN_FRAMES)
+#   dummy = raw_input("Press key for next loop...")
+ #   labeled_img = imshow_components(fgMask)
+ #   cv2.imshow('Labels', labeled_img)
     cv2.imshow('FGMask', fgMask)
     #show_video()
-    if cv2.waitKey(10) == 27:
+    if cv2.waitKey(30) & 0xff == 27:
         break
 
